aeffe14e2385538cd82132fa2845889821ee29f0
qingfenxiaochong <hanjun20@huawei.com>
2023-06-27 19:28:47 +0800

    change fused softmax condition size

--- megatron_npu/adaptor_model_fused_softmax.py
+++ megatron_npu/adaptor_model_fused_softmax.py
@@ -7,7 +7,7 @@ def is_kernel_available(self, mask, b, np, sq, sk):
     return (
             self.scaled_masked_softmax_fusion  # user want to fuse
             and self.input_in_float16  # input must be fp16
-            and 32 < sk <= 512  # sk must be 32 ~ 512
+            and 32 < sk <= 2048  # sk must be 32 ~ 2048
             and sq % 16 == 0  # sq must be divisor of 16
             and sk % 16 == 0  # sk must be divisor of 16
     )
