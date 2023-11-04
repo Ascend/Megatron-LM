0f53736fe31703332a413b53f39c9009287ba4cc
RyanAlexander <ruanzhixiang1@huawei.com>
2023-11-04 10:08:18 +0000

    !61 完善安全加固指南 排除tests下脚本的安全风险 校正test_gpt下脚本路径配置 * docs: 添加通信矩阵的说明 * docs: 添加日志相关的说明 * docs: 调整README说明 * docs: 调整安全加固方案 * refactor: 调整目录，使得文档执行步骤正确 * refactor: 对数据集目录及文件做权限控制 * docs: 调整安全加固选项 * Merge remote-tracking branch 'upstream/master' * Merge remote-tracking branch 'origin/master' * docs: 添加有关环境变量的说明 * !58 排除安全风险 完善安全加固指南 * Merge remote-tracking branch 'upstream/master' * feat: 去除文件全路径打印，排除安全风险 * feat: 控制生成文件index map files权限 * feat: 解决不安全的函数使用allow_pickle * feat: 清除具有风险的日志打印 * feat: 完善文件权限清单 * feat: 对数据集与index map生成、算子编译等场景做补充说明

--- tests/dataset_preprocess_t5.sh
+++ tests/dataset_preprocess_t5.sh
@@ -1,9 +1,14 @@
-# Step1: Concat json
-WIKI_DIR=./text
-OUTDIR=./output
+# Step1: 生成wiki_all.json文件
+
+# WikiExtractor生成text文件目录
+WIKI_DIR=./dataset/text
+# 生成数据集位置
+OUTDIR=./dataset/enwiki
 
 mkdir -p $OUTDIR
-rm $OUTDIR/wiki_all.json
+# 对数据集目录做权限控制，保障数据集文件处理过程中的安全
+chmod 750 $OUTDIR
+
 touch $OUTDIR/wiki_all.json
 
 find "$WIKI_DIR" -type f  -print0 |
@@ -16,8 +21,8 @@ find "$WIKI_DIR" -type f  -print0 |
             cat $new_name >> $OUTDIR/wiki_all.json
     done
 
-# Step2: Do Preprocess
-VOCAB=./bert-large-uncased-vocab.txt
+# Step2: 执行原生Megatron-LM的preprocess_data.py，实现数据集预处理
+VOCAB=./dataset/bert-large-uncased-vocab.txt
 python3 ../tools/preprocess_data.py \
        --input $OUTDIR/wiki_all.json \
        --output-prefix $OUTDIR/my-t5 \
@@ -27,3 +32,7 @@ python3 ../tools/preprocess_data.py \
        --split-sentences \
        --chunk-size 32 \
        --workers $(nproc)
+
+# 对数据集目录及文件做权限控制
+chmod -R 640 $OUTDIR
+chmod 750 $OUTDIR
